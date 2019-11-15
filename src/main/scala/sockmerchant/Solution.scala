aa7a56451c3bd92e70c476877334d5d4590fc3f6
Alessandro Badin <abadin@avaya.com>
2019-11-15 11:49:19 -0300

    Final version of sock merchant challenge

--- src/main/scala/sockmerchant/Solution.scala
+++ src/main/scala/sockmerchant/Solution.scala
@@ -5,17 +5,21 @@ object Solution {
   import scala.collection.mutable.HashSet
   import scala.collection.mutable.Set
   def main(args: Array[String]) {
-    import java.io.{BufferedReader, BufferedWriter, InputStreamReader, OutputStreamWriter}
+    import java.io.{BufferedReader, BufferedWriter, InputStreamReader, OutputStreamWriter, PrintWriter}
     val reader = new BufferedReader(new InputStreamReader(System.in))
     val writer = new BufferedWriter(new OutputStreamWriter(System.out))
-    val n = reader.readLine.map(_.toInt)
+    val n = reader.readLine.toInt
     val ar = reader.readLine.split(" ")
-    val control = new Array[Int](99)
-    val summarize = (position: Int, control: Array[Int], collors: Array[Int], acc: Int) => (control(ar(i)) + 1) % 2 match {
-      0 if position < 100 => summarize(position + 1, control, collors, acc + 1)
-      1 if position < 100 => summarize(position + 1, control, collors, acc)
-      _ => acc
+    def summarize(position: Int, control: Array[Int], acc: Int): Int = position match {
+      case `n` => acc
+      case _ => {
+        control(ar(position).toInt - 1) = (control(ar(position).toInt - 1) + 1) % 2
+        control(ar(position).toInt - 1) match {
+          case 0 => summarize(position + 1, control, acc + 1)
+          case 1 => summarize(position + 1, control, acc)
+        }
+      }
     }
-    return summarize(0, control, ar, 0)
+    println(summarize(0, new Array[Int](100), 0))
   }
 }
