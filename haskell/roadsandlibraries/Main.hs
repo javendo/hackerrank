5e3781e30025b314045eb2df6a1ec76788fc7c0d
Alessandro Badin <abadin@avaya.com>
2019-12-30 08:36:23 -0300

    Adding files to git ignore

--- haskell/roadsandlibraries/Main.hs
+++ haskell/roadsandlibraries/Main.hs
@@ -5,7 +5,6 @@ module Main where
 import Data.Array
 import Data.Set (Set, insert, member, empty)
 
-
 input :: String
 --input = "2\n3 3 2 1\n1 2\n3 1\n2 3\n6 6 2 5\n1 3\n3 4\n2 4\n1 2\n2 3\n5 6\n"
 input = "1\n5 3 6 1\n1 2\n1 3\n1 4\n"
@@ -14,7 +13,6 @@ l = [(1, 2), (1, 3), (1, 4)]
 --arr = accumArray (+) 0 ((1,1),(10000,10000)) ([((i, j), 1) | (i, j) <- l] ++ [((i, j), 1) | (j, i) <- l])
 --arr :: Array Int [Int]
 arr = accumArray (flip (:)) [] (1,6) (l ++ [(i, j) | (j, i) <- l])
-
 
 mkArr :: Int -> [(Int, Int)] -> Array Int [Int]
 mkArr size edges = accumArray (flip (:)) [] (1,size) (edges ++ [(i, j) | (j, i) <- edges])
