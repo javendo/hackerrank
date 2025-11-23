242acc4ac9433c2ad93e784fa7eceba3da60ef79
Alessandro Badin <784975+javendo@users.noreply.github.com>
2025-11-23 09:14:11 -0300

    adding longest consecutive code

--- python/longest_consecutive.py
+++ python/longest_consecutive.py
@@ -6,15 +6,17 @@ class Solution:
         consecutives = dict[int, (int, int)]()
         longest = (0, 1)
         for n in nums:
-            min_value = (consecutives.get(n - 1) and consecutives[n - 1][0]) or n
-            max_value = (consecutives.get(n + 1) and consecutives[n + 1][1]) or n
-            consecutives[n] = (min_value, max_value)
-            consecutives[min_value] = (consecutives[min_value][0], max_value)
-            consecutives[max_value] = (min_value, consecutives[max_value][1])
-            if ((consecutives[n][1] - consecutives[n][0]) > (longest[1] - longest[0])):
-                longest = consecutives[n]
-            print(consecutives)
+            if n not in consecutives.keys():
+                min_value = consecutives[n - 1][0] if consecutives.get(n - 1) else n
+                max_value = consecutives[n + 1][1] if consecutives.get(n + 1) else n
+                consecutives[n] = (min_value, max_value)
+                consecutives[min_value] = (consecutives[min_value][0], max_value)
+                consecutives[max_value] = (min_value, consecutives[max_value][1])
+                if ((consecutives[n][1] - consecutives[n][0]) > (longest[1] - longest[0])):
+                    longest = consecutives[n]
+                print(consecutives)
         return list(range(longest[0], longest[1] + 1))
  
 solution = Solution()
-print(solution.longestConsecutive([100, 4, 101, 1, 3, 2, 8, 98, 102, 99, 7, 5, 6, 1, 2]))
+#print(solution.longestConsecutive([100, 4, 101, 1, 3, 2, 8, 98, 102, 99, 7, 5, 6, 1, 2]))
+print(solution.longestConsecutive([-1,9,-3,-6,7,-8,-6,2,9,2,3,-2,4,-1,0,6,1,-9,6,8,6,5,2]))
